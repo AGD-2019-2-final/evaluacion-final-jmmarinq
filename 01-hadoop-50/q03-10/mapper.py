8f701a1f581ebba5827728ae5801302d9b89c550
jmmarinq <jmmarinq@unal.edu.co>
2020-01-25 22:41:56 -0500

    Update mapper.py

--- 01-hadoop-50/q03-10/mapper.py
+++ 01-hadoop-50/q03-10/mapper.py
@@ -2,3 +2,11 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+if __name__ == "__main__":
+    for line in sys.stdin:
+        line = line.strip()
+        line = line.split(',')
+        if len(line) >=2:
+            purpose = line[0]
+            amount = line[1]
+            sys.stdout.write("{}\t{}\t\n".format(purpose, amount))
