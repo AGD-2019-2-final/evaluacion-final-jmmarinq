36d10720a182b40634c56777c18c7d794af9a2ba
jmmarinq <jmmarinq@unal.edu.co>
2020-01-25 23:02:05 -0500

    Update reducer.py

--- 01-hadoop-50/q05-10/reducer.py
+++ 01-hadoop-50/q05-10/reducer.py
@@ -2,4 +2,18 @@ import sys
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
-
+if __name__ == '__main__':
+    Month = {}
+    for line in sys.stdin:
+        line = line.strip()
+        k = line.split(',')[0]
+        count = line.split(',')[1]
+        count=int(count)
+        if k in Month.keys():
+            Month[k].append(count)
+        else:
+            Month[k]=[]
+            Month[k].append(count)
+    for k in Month.keys():
+        suml =sum(Month[k])
+        sys.stdout.write("{}\t{}\n".format(k, suml))
