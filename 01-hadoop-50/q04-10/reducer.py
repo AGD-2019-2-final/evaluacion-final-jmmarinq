bb60ed8442eb305af1da52cc7f80e5ad11168bb4
jmmarinq <jmmarinq@unal.edu.co>
2020-01-26 17:41:53 -0500

    Update reducer.py

--- 01-hadoop-50/q04-10/reducer.py
+++ 01-hadoop-50/q04-10/reducer.py
@@ -1,19 +1,16 @@
 import sys
-#
-# >>> Escriba el codigo del reducer a partir de este punto <<<
-#
 if __name__ == '__main__':
-    Letter = {}
+    Letra = {}
     for line in sys.stdin:
         line = line.strip()
         k = line.split(',')[0]
-        count = line.split(',')[1]
-        count=int(count)
-        if k in Letter.keys():
-            Letter[k].append(count)
+        conta = line.split(',')[1]
+        conta=int(conta)
+        if k in Letra.keys():
+            Letra[k].append(conta)
         else:
-            Letter[k]=[]
-            Letter[k].append(count)
-    for k in Letter.keys():
-        suml =sum(Letter[k])
+            Letra[k]=[]
+            Letra[k].append(conta)
+    for k in Letra.keys():
+        suml =sum(Letra[k])
         sys.stdout.write("{},{}\n".format(k, suml))
