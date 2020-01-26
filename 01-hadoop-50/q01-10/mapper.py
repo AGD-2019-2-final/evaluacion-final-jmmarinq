e03c725c10e3ce76a1a2e6c822fdca3cb24ca830
jmmarinq <jmmarinq@unal.edu.co>
2020-01-26 17:10:23 -0500

    Update mapper.py

--- 01-hadoop-50/q01-10/mapper.py
+++ 01-hadoop-50/q01-10/mapper.py
@@ -1,13 +1,8 @@
 import sys
-#
-# >>> Escriba el codigo del mapper a partir de este punto <<<
-#
 if __name__ == '__main__':
     for line in sys.stdin:
         line = line.strip()
         line = line.split(',')
-
         if len(line) >=2:
-            credit_history = line[2]
-
-            sys.stdout.write("{}\t1\n".format(credit_history))
+            credito = line[2]
+            sys.stdout.write("{}\t1\n".format(credito))
