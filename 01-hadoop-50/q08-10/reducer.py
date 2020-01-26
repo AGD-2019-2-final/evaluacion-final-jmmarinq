3c18a7a9d41e5a6d93217b5a5a3ab16df6401169
jmmarinq <jmmarinq@unal.edu.co>
2020-01-26 18:10:34 -0500

    Update reducer.py

--- 01-hadoop-50/q08-10/reducer.py
+++ 01-hadoop-50/q08-10/reducer.py
@@ -1,4 +1,17 @@
 import sys
-#
-#  >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+if __name__ == '__main__':
+    Letra = {}
+    for line in sys.stdin:
+        line = line.strip()
+        k = line.split(',')[0]
+        valor = line.split(',')[1]
+        valor=float(valor)
+        if k in Letra.keys():
+            Letra[k].append(valor)
+        else:
+            Letra[k]=[]
+            Letra[k].append(valor)
+    for k in Letra.keys():
+        suml =sum(Letra[k])
+        avgl =suml/float(len(Letra[k]))
+        sys.stdout.write("{}\t{}\t{}\n".format(k, suml, avgl))
