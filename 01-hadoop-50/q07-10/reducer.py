15927ce441a48d3a995934e88ad00368287950d4
jmmarinq <jmmarinq@unal.edu.co>
2020-01-26 00:04:29 -0500

    Update reducer.py

--- 01-hadoop-50/q07-10/reducer.py
+++ 01-hadoop-50/q07-10/reducer.py
@@ -2,14 +2,11 @@ import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
 if __name__ == '__main__':
-    ordered = []
+
     for line in sys.stdin:
-        l = line.split(',')[0]
-        d = line.split(',')[1]
-        v = line.split(',')[2]
-        v = int(v)
-        ordered.append((l,d,v))
-    sorted(ordered, key=lambda element: (element[2]))
-    for l,d,v in ordered:
-        sys.stdout.write("{}\t{}\t{}\n".format(l,d,v))
+        clave,valor_format,fecha,valor = line.split("\t")
+        valor=int(valor)
+        
+        sys.stdout.write("{}   {}   {}\n".format(clave, fecha, valor))
