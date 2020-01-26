709cdfd0d45857bb1d9980b9ccae5180ab4f4d9a
jmmarinq <jmmarinq@unal.edu.co>
2020-01-25 22:32:43 -0500

    Update reducer.py

--- 01-hadoop-50/q02-10/reducer.py
+++ 01-hadoop-50/q02-10/reducer.py
@@ -2,3 +2,21 @@ import sys
 #
 #  >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+
+if __name__ == '__main__':
+
+    purpose_1={}
+    #total = 0
+    for line in sys.stdin:
+        line = line.strip()
+        purpose = line.split('\t')[0]
+        amount = line.split('\t')[1]
+        amount=int(amount)
+        if purpose in purpose_1.keys():
+          purpose_1[purpose].append(amount)
+        else:
+          purpose_1[purpose]=[]
+          purpose_1[purpose].append(amount)
+    for purpose in purpose_1.keys():
+        sump=max(purpose_1[purpose])
+        sys.stdout.write("{}\t{}\t\n".format(purpose, sump))
