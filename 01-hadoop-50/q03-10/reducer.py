ed8e9daa236fb6d00f618bfe804864dfb24df2d0
jmmarinq <jmmarinq@unal.edu.co>
2020-01-25 22:42:14 -0500

    Update reducer.py

--- 01-hadoop-50/q03-10/reducer.py
+++ 01-hadoop-50/q03-10/reducer.py
@@ -2,3 +2,19 @@ import sys
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+if __name__ == '__main__':
+    ## cada linea de texto recibida es una
+    ## entrada clave \\tabulador valor
+    b=[]
+    for line in sys.stdin:
+        line = line.strip()
+        line = line.split('\t')
+        purpose = line[0]
+        amount = line[1]
+        amount=int(amount)
+        b.append((purpose,amount))
+        
+       ## sys.stdout.write(\"{}\\n\".format(sorted(line)))
+    b.sort(key=lambda b: b[1])
+    for i in b:
+       sys.stdout.write("{}\t\n".format((i[0] + "," + str(i[1]))))
