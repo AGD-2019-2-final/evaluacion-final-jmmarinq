fc9b49e0aefc4b2a606e225807a191dd6183bdeb
jmmarinq <jmmarinq@unal.edu.co>
2020-01-29 20:48:52 -0500

    Update reducer.py

--- 01-hadoop-50/q07-10/reducer.py
+++ 01-hadoop-50/q07-10/reducer.py
@@ -1,8 +1,6 @@
 import sys
-import operator
-from operator import itemgetter
 if __name__ == '__main__':   
     for line in sys.stdin:
-        codigo,letraa, fechaa, valora = line.split(",")
-        valora = int(valora)
-        sys.stdout.write("{}\t{}\t{}\n".format(letraa,fechaa,valora))
+        concat,key,date,val= line.split("\t")
+        val=int(val)
+        sys.stdout.write("{}   {}   {}\n".format(key,date ,val))
